fafc8cf9379e573ad6d135076a097587398822fa
KorshakVladimir <korshak.vladimir.v@gmail.com>
2015-06-15 18:18:50 +0300

    cgm.js

--- static/js/cgm.js
+++ static/js/cgm.js
@@ -10,3 +10,22 @@ $(window).bind("load", function () {
     }
 
 })
+
+function f_test () {
+	console.log('good');
+	// document.querySelect
+	doc_el = document.documentElement;
+	el_backg =   doc_el.querySelector('.backg');
+	if (!el_backg) {
+		return;
+	}
+	height_document = doc_el.clientHeight;
+	if (height_document < el_backg.clientHeight){
+		el_backg.style.top = height_document + 15 + "px"
+	}
+}
+
+
+document.documentElement.addEventListener('onload',f_test());
+
+// document.onload = f_test()
